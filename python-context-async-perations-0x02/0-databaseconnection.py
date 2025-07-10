6c45e4e2a8c065ef034dec8f573f5dca9ffe5486
Ali Maamoun <alimaamoon1@gmail.com>
2025-07-10 18:56:19 +0300

    create a class based context manager to handle opening and closing database connections automatically

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+class DatabaseConnection:
+    """
+    Custom context manager for handling SQLite database connections.
+    """
+
+    def __init__(self, db_name):
+        self.db_name = db_name
+        self.conn = None
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db_name)
+        return self.conn
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.conn:
+            self.conn.close()
